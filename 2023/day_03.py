10f58530a8cb8c0608750c66a69e064c5e3c844e
German Bravo Lopez <german.bravo.lopez@asml.com>
2023-12-06 13:02:28 +0100

    use nice implementation of using dict and intersection of tuples in python

--- 2023/day_03.py
+++ 2023/day_03.py
@@ -1,62 +1,16 @@
-def is_valid(row, col, rows, cols):
-    return 0 <= row < rows and 0 <= col < cols
+import math as m, re
 
-def has_symbol_around(engine_schematic, row, col, rows, cols):
-    directions = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)]
-    for dr, dc in directions:
-        new_row, new_col = row + dr, col + dc
-        if is_valid(new_row, new_col, rows, cols) and engine_schematic[new_row][new_col] in "*+-=@#$%&/":
-            return True
-    return False
+board = list(open('day_03_input.txt'))
+chars = {(r, c): [] for r in range(140) for c in range(140)
+                    if board[r][c] not in '01234566789.'}
 
-def get_part_number(engine_schematic, row, col):
-    prev_idx, prev_found, next_idx, next_found = 0, 0, 0, 0
+for r, row in enumerate(board):
+    for n in re.finditer(r'\d+', row):
+        edge = {(r, c) for r in (r-1, r, r+1)
+                       for c in range(n.start()-1, n.end()+1)}
 
-    for i in range(4):
-        if not prev_found and not engine_schematic[row][col-i].isdigit():
-            prev_idx = i - 1
-            prev_found = 1
-        if not next_found and not engine_schematic[row][col+i].isdigit():
-            next_idx = i
-            next_found = 1
-    part_number_str = ""
-    for i in range(-prev_idx, next_idx):
-        part_number_str = part_number_str + engine_schematic[row][col+i]
-    return part_number_str
+        for o in edge & chars.keys():
+            chars[o].append(int(n.group()))
 
-
-def sum_of_part_numbers(engine_schematic, output_file):
-    rows = len(engine_schematic)
-    cols = len(engine_schematic[0]) - 1
-
-    total_sum = 0
-    with open(output_file, 'w') as file:
-        for row in range(rows):
-            col = 0
-            while col < cols:
-                if engine_schematic[row][col].isdigit():
-                    tmp_part_number = get_part_number(engine_schematic, row, col)
-                    if has_symbol_around(engine_schematic, row, col, rows, cols):
-                        total_sum += int(tmp_part_number)
-                        if not engine_schematic[row][col+1].isdigit():
-                            col += 1
-                        elif not engine_schematic[row][col+2].isdigit():
-                            col += 2
-                        else:
-                            col += 3
-                        # Debugging:
-                        message = f"Row: {row}, Col: {col}, Part Number: {tmp_part_number}\n"
-                        file.write(message)
-                    else:
-                        col += 1
-                else:
-                    col += 1
-
-    return total_sum
-
-# Read input from file
-with open('day_03_input.txt', 'r') as file:
-    engine_schematic = file.readlines()
-
-result = sum_of_part_numbers(engine_schematic, 'day_03_output.txt')
-print(result)
+print(sum(sum(p)    for p in chars.values()),
+      sum(m.prod(p) for p in chars.values() if len(p)==2))
