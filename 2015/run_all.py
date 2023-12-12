f2615492c4c8580aef4d4ba796b908302bafd7f1
German Bravo Lopez <gebralo@hotmail.com>
2023-12-12 21:31:24 +0100

    make run_all same documents and have results in a text file outside

--- 2015/run_all.py
+++ 2015/run_all.py
@@ -5,28 +5,28 @@ if len(sys.argv) == 1:
 elif len(sys.argv) == 2:
     advent_days=[f'{int(sys.argv[1]):02}']
 else:
-    print("Only 1 argument supported, it must specify the day to run...")
+    print('Only 1 argument supported, it must specify the day to run...')
     sys.exit()
 
-results = [[74, 1795],
-           [1586300, 3737498],
-        ]
+results = [[int(item) for item in r.split(',')] for r in open('results.txt').read().splitlines()]
+if len(results) != len(advent_days):
+    print('Number of results and inputs files are not matching')
+    sys.exit()
 
 for day in advent_days:
     script_path = f'day_{day}.py'
+    print(f'Running day {day}...')
     result = subprocess.run(['python', script_path], capture_output=True, text=True)
 
     if result.returncode == 0 and result.stdout != '':
         if results[int(day)-1][0] == int(result.stdout.split()[0]) and results[int(day)-1][1] == int(result.stdout.split()[1]):
-            print(f"Day {day} is passing...")
-            print(f"\tResults: {results[int(day)-1][0]}, {results[int(day)-1][1]}")
+            print(f'\t[Passed] results: {results[int(day)-1][0]}, {results[int(day)-1][1]}')
         else:
-            print(f"Day {day}:")
-            print(f"[P1] expected output:\t{results[int(day)-1][0]}")
-            print(f"[P1] calculated output:\t{result.stdout.split()[0]}")
-            print(f"[P2] expected output:\t{results[int(day)-1][1]}")
-            print(f"[P2] calculated output:\t{result.stdout.split()[1]}")
+            print(f'\t[P1] expected output:\t{results[int(day)-1][0]}')
+            print(f'\t[P1] calculated output:\t{result.stdout.split()[0]}')
+            print(f'\t[P2] expected output:\t{results[int(day)-1][1]}')
+            print(f'\t[P2] calculated output:\t{result.stdout.split()[1]}')
     else:
-        print(f"Error running {script_path}. Exit code:", result.returncode)
-        print("Error output:", result.stderr)
-        print("Script output:", result.stdout)
+        print(f'\tError running {script_path}. Exit code:', result.returncode)
+        print('\tError output:', result.stderr)
+        print('\tScript output:', result.stdout)
--- 2023/run_all.py
+++ 2023/run_all.py
@@ -5,38 +5,28 @@ if len(sys.argv) == 1:
 elif len(sys.argv) == 2:
     advent_days=[f'{int(sys.argv[1]):02}']
 else:
-    print("Only 1 argument supported, it must specify the day to run...")
+    print('Only 1 argument supported, it must specify the day to run...')
     sys.exit()
 
-results = [[54390, 54277],
-           [2169, 60948],
-           [540131, 86879020],
-           [18619, 8063216],
-           [836040384, 10834440],
-           [345015, 42588603],
-           [251029473, 251003917],
-           [22199, 13334102464297],
-           [1853145119, 923],
-           [6768, 351],
-           [9686930, 630728425490],
-           [6852, 8475948826693]
-        ]
+results = [[int(item) for item in r.split(',')] for r in open('results.txt').read().splitlines()]
+if len(results) != len(advent_days):
+    print('Number of results and inputs files are not matching')
+    sys.exit()
 
 for day in advent_days:
     script_path = f'day_{day}.py'
+    print(f'Running day {day}...')
     result = subprocess.run(['python', script_path], capture_output=True, text=True)
 
     if result.returncode == 0 and result.stdout != '':
         if results[int(day)-1][0] == int(result.stdout.split()[0]) and results[int(day)-1][1] == int(result.stdout.split()[1]):
-            print(f"Day {day} is passing...")
-            print(f"\tResults: {results[int(day)-1][0]}, {results[int(day)-1][1]}")
+            print(f'\t[Passed] results: {results[int(day)-1][0]}, {results[int(day)-1][1]}')
         else:
-            print(f"Day {day}:")
-            print(f"[P1] expected output:\t{results[int(day)-1][0]}")
-            print(f"[P1] calculated output:\t{result.stdout.split()[0]}")
-            print(f"[P2] expected output:\t{results[int(day)-1][1]}")
-            print(f"[P2] calculated output:\t{result.stdout.split()[1]}")
+            print(f'\t[P1] expected output:\t{results[int(day)-1][0]}')
+            print(f'\t[P1] calculated output:\t{result.stdout.split()[0]}')
+            print(f'\t[P2] expected output:\t{results[int(day)-1][1]}')
+            print(f'\t[P2] calculated output:\t{result.stdout.split()[1]}')
     else:
-        print(f"Error running {script_path}. Exit code:", result.returncode)
-        print("Error output:", result.stderr)
-        print("Script output:", result.stdout)
+        print(f'\tError running {script_path}. Exit code:', result.returncode)
+        print('\tError output:', result.stderr)
+        print('\tScript output:', result.stdout)
