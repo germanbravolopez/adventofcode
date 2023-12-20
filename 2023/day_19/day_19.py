d141adf4ae541feaf3082b92d425e172e2f293f4
German Bravo Lopez <gebralo@hotmail.com>
2023-12-20 17:22:07 +0100

    reorganize code and add some comments

--- 2023/day_19/day_19.py
+++ 2023/day_19/day_19.py
@@ -1,12 +1,14 @@
 import re
 
+
+# MyParts format: each objet has attributes: {x, m, a, s, status}
 class MyParts:
     def __init__(self, x, m, a, s):
         self.x = x
         self.m = m
         self.a = a
         self.s = s
-        self.status = None # Initially, the acceptance status is set to None
+        self.status = None  # Initially, the acceptance status is set to None
 
     def __str__(self):
         return f"x: {self.x}, m: {self.m}, a: {self.a}, s: {self.s}, status: {self.status}"
@@ -18,12 +20,41 @@ class MyParts:
             print("Invalid status. Please provide 'A' or 'R'.")
 
 
-input = open('input.txt').read().strip().split('\n\n')
+# Returns True if the part is compliant with the condition
+def parse_condition(p, cond):
+    match re.split(r'[<>]', cond)[0]:
+        case 'x':
+            if (('<' in cond and p.x < int(re.split(r'[<>]', cond)[1])) or
+                    ('>' in cond and p.x > int(re.split(r'[<>]', cond)[1]))):
+                return True
+            else:
+                return False
+        case 'm':
+            if (('<' in cond and p.m < int(re.split(r'[<>]', cond)[1])) or
+                    ('>' in cond and p.m > int(re.split(r'[<>]', cond)[1]))):
+                return True
+            else:
+                return False
+        case 'a':
+            if (('<' in cond and p.a < int(re.split(r'[<>]', cond)[1])) or
+                    ('>' in cond and p.a > int(re.split(r'[<>]', cond)[1]))):
+                return True
+            else:
+                return False
+        case 's':
+            if (('<' in cond and p.s < int(re.split(r'[<>]', cond)[1])) or
+                    ('>' in cond and p.s > int(re.split(r'[<>]', cond)[1]))):
+                return True
+            else:
+                return False
 
-workflows_input = input[0].split('\n')
-parts_input = input[1].split('\n')
 
-# Organize workflows in dict
+data = open('input.txt').read().strip().split('\n\n')
+
+workflows_input = data[0].split('\n')
+parts_input = data[1].split('\n')
+
+# Organize workflows in dict: {'label': [[cond1:dest1], [cond2:dest2], [dest3]]}
 workflows = dict()
 for workflow in workflows_input:
     steps = []
@@ -33,61 +64,25 @@ for workflow in workflows_input:
         else:
             steps.append([step[:-1]])
     workflows[workflow.split('{')[0]] = steps
-#print(workflows)
-
-# Organize parts in a class
+# Organize parts in MyParts class
 parts = []
 for part in parts_input:
     parts.append(MyParts(int(re.findall(r'\d+', part.split(',')[0])[0]),
                          int(re.findall(r'\d+', part.split(',')[1])[0]),
                          int(re.findall(r'\d+', part.split(',')[2])[0]),
                          int(re.findall(r'\d+', part.split(',')[3])[0])))
-#print(parts[0].x)
-
-# Classify parts
-def parse_condition(part, condition):
-    match re.split(r'<|>', condition)[0]:
-        case 'x':
-            if (('<' in condition and part.x < int(re.split(r'<|>', condition)[1])) or
-                ('>' in condition and part.x > int(re.split(r'<|>', condition)[1]))):
-                return True
-            else:
-                return False
-        case 'm':
-            if (('<' in condition and part.m < int(re.split(r'<|>', condition)[1])) or
-                ('>' in condition and part.m > int(re.split(r'<|>', condition)[1]))):
-                return True
-            else:
-                return False
-        case 'a':
-            if (('<' in condition and part.a < int(re.split(r'<|>', condition)[1])) or
-                ('>' in condition and part.a > int(re.split(r'<|>', condition)[1]))):
-                return True
-            else:
-                return False
-        case 's':
-            if (('<' in condition and part.s < int(re.split(r'<|>', condition)[1])) or
-                ('>' in condition and part.s > int(re.split(r'<|>', condition)[1]))):
-                return True
-            else:
-                return False
-
+# Set status for each part
 for part in parts:
     current_step = 'in'
-    while not current_step in ['A', 'R']:
-        # First step is 'in'
-        #print(current_step, workflows.get(current_step))
+    while current_step not in ['A', 'R']:
         for step in workflows.get(current_step):
             if len(step) > 1:
-                #print(parse_condition(part, step[0]))
                 if parse_condition(part, step[0]):
                     current_step = step[1]
                     break
             else:
-                #print(step[0])
                 current_step = step[0]
     part.set_status(current_step)
-
 # Calculate all
 print(sum(part.x + part.m + part.a + part.s for part in parts if part.status == 'A'))
 print(-1)
