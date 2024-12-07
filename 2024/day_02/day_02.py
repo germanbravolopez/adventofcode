8da81d4f15f446f7c384ee61b34b00638dcafef5
German Bravo Lopez <gebralo@hotmail.com>
2024-12-07 14:52:47 +0100

    part 1 done

--- 2024/day_02/day_02.py
+++ 2024/day_02/day_02.py
@@ -1,3 +1,26 @@
+# Condition 1: all increasing or all decreasing
+def is_cond_1 (levels):
+    increasing = True
+    for i in range(len(levels)-1):
+        if not(levels[i] < levels[i+1]):
+            increasing = False
+    if not(increasing):
+        for i in range(len(levels)-1):
+            if not(levels[i] > levels[i+1]):
+                return False
+        return True
+    else:
+        return True
+
+# Condition 2: distance between levels is in range(1,3)
+def is_cond_2 (levels):
+    for i in range(len(levels)-1):
+        if not(abs(levels[i] - levels[i+1]) <= 3):
+            return False
+    return True
+
 # Read input from file
 with open('input.txt', 'r') as file:
-    list_1, list_2 = map(list, zip(*(map(int, line.strip().split()) for line in file)))
+    reports = [list(map(int, line.strip().split())) for line in file]
+
+print(sum(1 for report in reports if is_cond_1(report) and is_cond_2(report)))
