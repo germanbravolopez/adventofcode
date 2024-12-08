c7ff0eacc09ef64a39df1cd1554b31c5db889097
German Bravo Lopez <gebralo@hotmail.com>
2024-12-08 21:46:30 +0100

    day 3 from 2015 updated previously and not commited yet

--- 2015/day_03/day_03.py
+++ 2015/day_03/day_03.py
@@ -4,7 +4,13 @@ map_addr = {'^': (0,1),
             '>': (1,0),
             '<': (-1,0),
             'v': (0,-1)}
-start_addr = (0,0)
-list_addr = [start_addr]
-print(-1)
-print(-1)
+
+current_addr = (0,0)
+list_addr = [current_addr]
+
+for i in range(len(f)):
+    current_addr = tuple(map(lambda i, j: i + j, current_addr, map_addr.get(f[i])))
+    if not current_addr in list_addr:
+        list_addr.append(current_addr)
+
+print(len(list_addr))
